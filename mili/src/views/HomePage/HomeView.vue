c9482b460415956d35057c4e4f4f082b9d56da8f
Harbour% <2358272468@qq.com>
2022-04-24 20:51:37 +0800

    框架

---
+++ mili/src/views/HomePage/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <Header />
+</template>
+<script>
+import Header from '@/components/HomePage/Header.vue'
+export default {
+    components: {Header}
+}
+</script>
+<style>
+
+</style>
---
+++ milimili/src/views/HomePage/HeadView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        zhuye
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
