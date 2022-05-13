fcfa427f5f6e1fcdbf8c9aba41099eef390f64a2
ZQRuii <89249466+ZQRuii@users.noreply.github.com>
2022-05-13 14:25:48 +0800

    PCMV

--- mili/src/components/PC/PCmanageVideo.vue
+++ mili/src/components/PC/PCmanageVideo.vue
@@ -14,9 +14,36 @@
               </div>
             </div>
           </el-tab-pane>
-          <el-tab-pane label="自动审核" name="second"> </el-tab-pane>
-          <el-tab-pane label="人工审核" name="third"> </el-tab-pane>
-          <el-tab-pane label="已通过" name="fourth"> </el-tab-pane>
+          <el-tab-pane label="自动审核" name="second">
+            <div class="vv">
+              <div
+                v-for="(item, index) in VideoArrayAuditing"
+                :key="index"
+                style="margin-left: 10px; margin-top: 10px"
+              >
+                <Video :singleVideo="item" />
+              </div></div
+          ></el-tab-pane>
+          <el-tab-pane label="人工审核" name="third">
+            <div class="vv">
+              <div
+                v-for="(item, index) in VideoArrayAudit"
+                :key="index"
+                style="margin-left: 10px; margin-top: 10px"
+              >
+                <Video :singleVideo="item" />
+              </div></div
+          ></el-tab-pane>
+          <el-tab-pane label="已通过" name="fourth">
+            <div class="vv">
+              <div
+                v-for="(item, index) in VideoArrayAudited"
+                :key="index"
+                style="margin-left: 10px; margin-top: 10px"
+              >
+                <Video :singleVideo="item" />
+              </div></div
+          ></el-tab-pane>
         </el-tabs>
       </div>
     </el-card>
@@ -32,13 +59,16 @@ export default {
       activeName: "first",
       jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMCwiaXNTdXBlckFkbWluIjp0cnVlfQ.qaTIp4fibthTzo72_Yc3a0iTkWiSm-ESpza_ISYbsnU",
       VideoArrayMain: [],
+      VideoArrayAudited: [],
+      VideoArrayAuditing: [],
+      VideoArrayAudit: [],
     };
   },
   created() {
     var that = this;
     this.$axios({
       method: "post",
-      url: "https://milimili.super2021.com/api/user/video-list",
+      url: "https://milimili.super2021.com/api/user/video-audit-list",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
       },
@@ -50,7 +80,11 @@ export default {
         console.log(res);
         console.log(res.data.video_list);
         that.VideoArrayMain = res.data.video_list;
+        that.VideoArrayAudited = res.data.video_list_audited;
+        that.VideoArrayAuditing = res.data.video_list_auditing;
+        that.VideoArrayAudit = res.data.video_list_need_audit;
         console.log(this.VideoArrayMain);
+        console.log(this.VideoArrayAudited);
         that.len = this.VideoArrayMain.length;
         console.log(that.len);
       })
@@ -73,8 +107,7 @@ export default {
 }
 
 .vv {
-  width: 100%;
-  height: 50vh;
+  width: 98%;
   display: flex;
   flex-wrap: wrap;
   /* justify-content: space-between; */
